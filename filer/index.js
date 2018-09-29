4fe10a62c0b17e2fa767b33c0b5fde8b09d439f0
fugroup <vidar@fugroup.net>
2018-09-29 03:11:43 +0200

    Filer working

--- filer/index.js
+++ filer/index.js
@@ -1,8 +1,30 @@
+const path = require('path')
 const log = require('rainlog')
 const Dugg = require('dugg')
-const yargs = require('yargs')
 const loader = require('conficurse')
-const config = loader.load('config')
-const files = yargs.argv._
+const yargs = require('yargs')
 
+// Collect file names
+const files = yargs.argv._
 console.log(files)
+
+// Load config
+const config = loader.load('config.yml')
+Dugg.config(config)
+
+// Upload files
+const run = async (file) => {
+  const upload = new Dugg(file)
+  try {
+    const res = await upload.s3({ bucket: '7ino' })
+    log.green(`${log.pp(res)}`)
+    console.log(res['Location'])
+  } catch (err) {
+    log.red(`Upload error: ${err.message}`)
+  }
+}
+
+for (const file of files) {
+  const name = path.basename(file)
+  run({ name, path: file })
+}
